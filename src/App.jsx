3871914ee9cb26d4455e6bd13245f32c43c48840
sacord777 <santiagocordeiro10@gmail.com>
2025-05-28 20:01:39 -0300

    Fixing search bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Search from './components/search.jsx'
+import Search from './components/Search.jsx'
 import Spinner from './components/Spinner.jsx'
 import MovieCard from './components/MovieCard.jsx';
 import { useDebounce } from 'react-use';
